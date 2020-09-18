0a56c0c844728acc58b798f595ca6f13519bce26
Elijah Brooks <elijahcbrooks@gmail.com>
2020-09-18 18:28:30 +0000

    Automatically backed up by Learn

--- lib/updating_nested_hashes.rb
+++ lib/updating_nested_hashes.rb
@@ -45,7 +45,12 @@ def add_characters
          {name: "Steven", age: "30", attitude: "confused"},
          {name: "Nurse", age: "44", attitude: "worried"}
          ]
-      }
+      },
+      :additional_characters => [
+        
+        {name: "Prince Ascalus"},
+        {name: "Apothercary"}
+        ]
    }
 
    # Write code below that does the following programmatically:
